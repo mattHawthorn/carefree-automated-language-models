ab43481428046349ee27e321e4c5d1f6015edead
mattHawthorn <mhawthorn@ccri.com>
2016-11-30 22:42:03 -0500

    timeseries analyzer only shows plot if you tell it to

--- calm/timeseries.py
+++ calm/timeseries.py
@@ -1,25 +1,66 @@
 #coding:utf-8
 
 from matplotlib import pyplot as plt
+from matplotlib.colors import cnames
+from itertools import cycle
 from math import ceil, floor
 from datetime import datetime, date
+
 from calm.objects import BagOfWords
 from calm.corpus import BagOfWordsCorpus
+from datetime import datetime, date, time, timedelta
 
+EPOCH = datetime(1970,1,1)
+
+defaultcolors = ["blue",
+                "red",
+                "green",
+                "orange",
+                "black",
+                "lightblue",
+                "salmon",
+                "yellowgreen",
+                "gold",
+                "gray",
+                "midnightblue",
+                "firebrick",
+                "darkolivegreen",
+                "darkgoldenrod",
+                "lightgray"]
+
+
+def getTotalWords(doc):
+    return doc.bagOfWords.total
 
 class TokenTimeSeriesAnalyzer:
     def __init__(self,corpus,time_field,converter = lambda x:x):
         self.corpus = corpus
         self.time_field = time_field
-        self.converter = converter
         
-        self.allCounts = BagOfWords()
-        counts = ((t, doc.bagOfWords.total) for doc,t in self.docTimeIter())
-        self.allCounts._addmanyCounts(counts)
+        if type(converter) is dict:
+            if "default" not in converter:
+                converters = converter.copy()
+                converters["default"] = next(iter(converter.values()))
+            self.converters = converters
+        elif callable(converter):
+            self.converters = dict(default=converter)
+        else:
+            raise ValueError("converter must be a callable or dict of callables, transforming the docs' time_field attribute to a plottable quantity.")
         
-    def docTimeIter(self):
+        self.allCounts = {}
+        for converter, time_f in self.converters.items():
+            self.allCounts[converter] = self.bagOfTimes(converter, counter=lambda doc: doc.bagOfWords.total)
+    
+    def bagOfTimes(self, converter="default", counter=getTotalWords):
+        bag_of_times = BagOfWords()
+        bag_of_times._addmanyCounts((t, counter(doc)) for doc,t in self.docTimeIter(converter))
+        return bag_of_times
+    
+    def docTimeIter(self, converter="default"):
         time_field = self.time_field
-        converter = self.converter
+        
+        if not callable(converter):
+            converter = self.converters[converter]
         
         for doc in self.corpus:
             if doc.__hasattr__(time_field):
@@ -29,7 +70,7 @@ class TokenTimeSeriesAnalyzer:
             else:
                 continue
     
-    def series(self, token, normalize = True, min_time=None, max_time=None):
+    def series(self, token, converter="default", normalize=True, min_time=None, max_time=None, label = None, docfilter=None):
         if min_time is None:
             if max_time is None:
                 validate = lambda doc, token, t: tokenID in doc.bagOfWords
@@ -40,27 +81,73 @@ class TokenTimeSeriesAnalyzer:
                 validate = lambda doc, token, t: tokenID in doc.bagOfWords and min_time <= t
             else:
                 validate = lambda doc, token, t: tokenID in doc.bagOfWords and min_time <= t and t <= max_time
-        
+        if docfilter is not None:
+            validate = lambda doc: docfilter(doc) and validate(doc)
+            
+        if label is None:
+            label = token
+            
         tokenID = self.corpus.vocab.ID[token]
-        counts = ((t, doc.bagOfWords[tokenID]) for doc,t in self.docTimeIter() if validate(doc, tokenID, t))
+        counts = ((t, doc.bagOfWords[tokenID]) for doc,t in self.docTimeIter(converter) if validate(doc, tokenID, t))
         token_counts = BagOfWords()
         token_counts._addmanyCounts(counts)
         
         if len(token_counts) > 0:
-            allCounts = self.allCounts
+            if not callable(converter):
+                allCounts = self.allCounts[converter]
+            else:
+                allCounts = self.bagOfTimes(converter, counter=lambda doc: doc.bagOfWords.total)
+            
             if normalize:
                 token_counts = {time:count/allCounts[time] for time,count in token_counts.items()}
+            
             return tuple(zip(*sorted(list(token_counts.items()))))
         else:
             return ((),())
         
-    def plot(self, token, normalize=True, min_time=None, max_time=None):
-        times,counts = self.series(token, normalize=normalize, min_time=min_time, max_time=max_time)
+    def compareTokens(self, tokens, converter="default", 
+                      normalize=True, min_time=None, max_time=None, title=None, labels=None, colors=defaultcolors):
+        if labels is None:
+            labels = tokens
+            
+        series_iter = (self.series(token, converter=converter, 
+                                   normalize=normalize, 
+                                   min_time=min_time, max_time=max_time) for token in tokens)
+        self.compareSeries(series=series_iter, labels=labels, title=title, colors=colors)
+
+    def compareSeries(self, series, labels=None, title=None, colors=defaultcolors, show=False):
+        if labels is None:
+            labels = map(str, range(len(series)))
+        maxcount = 0.0
+        fig, ax = plt.subplots()
+        
+        for color, label, (times, counts) in zip(cycle(colors), labels, series):
+            maxcount = max(maxcount, max(counts))
+            ax.plot(times,counts,color=color,label=label)
+            
+        ax.set_ylim(0,1.1*maxcount)
+        if title is not None:
+            ax.set_title(title)
+        plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
+        plt.xticks(rotation="vertical")
+        if show:
+            plt.show()
+        
+    def plot(self, token, converter="default", normalize=True, min_time=None, max_time=None, title=None, color=None):
+        times,counts = self.series(token, converter=converter, normalize=normalize, min_time=min_time, max_time=max_time,show=False)
         fig, ax = plt.subplots()
         #ax.xaxis.set_major_locator(mdates.YearLocator)
         ax.set_ylim(0,1.1*max(counts))
-        ax.plot(times,counts)
-        plt.show()
+        ax.plot(times,counts,color=color)
+        if title is None:
+            title = token
+        else:
+            title = " - ".join(token,title)
+        ax.set_title(title)
+        plt.xticks(rotation="vertical")
+        if show:
+            plt.show()
+
 
 
 def monthDT(dt):
